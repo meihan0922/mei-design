0f9a145fa03b1b553fbc64e0a4e8ebd089592748
Mei <meihen0922@gmail.com>
2022-06-10 12:13:38 +0800

    style(tailwind.config): add ui kit color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,31 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        tertiary: "#324d67",
-        secondary: "#5f5f5f",
-        primary: "#f02d34",
-        disable: "#ebebeb",
-        bg: "#dcdcdc",
+        chartColor: {
+          Blue: "#0011F6",
+          MainBlue: "#3240FF",
+          LightBlue: "#3C91FF",
+          Nautical: "#48E3FF",
+          Turquoise: "#0BFFDB",
+          LightViolet: "#BB9CFF",
+          Violet: "#8122FF",
+          Red: "#FD005C",
+          LightRed: "#FF8BAA",
+        },
+        StyleColors: {
+          MainGray: "#2A3F58",
+          DarkGray: "#72809D",
+          Gray: "#CCD2DE",
+          LightGray: "#FDF2F5",
+          LightBlue: "#F4F7FC",
+          GreyBlue: "#DBE9FF",
+        },
+        SystemColors: {
+          Success: "#A7E521",
+          Info: "#03C3EC",
+          Alert: "#FFD422",
+          Error: "#EF3F26",
+        },
       },
     },
   },
